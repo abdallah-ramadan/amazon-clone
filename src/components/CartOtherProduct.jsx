685ea43247c19955d7a4c49948f6decabcfc9c5a
Abdallah Ramadan <abdallahram777@gmail.com>
2025-08-16 16:03:42 +0300

    edit other products component styles

--- src/components/CartOtherProduct.jsx
+++ src/components/CartOtherProduct.jsx
@@ -3,8 +3,13 @@ import CartOtherProductItem from '../components/CartOtherProductItem'
 
 function CartOtherProduct() {
     return (
-        <div className=' bg-white hidden lg:block px-5 pt-4'>
+        <div className=' bg-white hidden lg:block px-5 pt-4 max-h-[500px] overflow-y-auto  [scrollbar-width:thin] [scrollbar-color:#9ca3af_#f3f4f6]'>
             <h2 className='mb-2.5 font-semibold text-[18px]'>Users also bought:</h2>
+            <CartOtherProductItem />
+            <CartOtherProductItem />
+            <CartOtherProductItem />
+            <CartOtherProductItem />
+            <CartOtherProductItem />
             <CartOtherProductItem />
             <CartOtherProductItem />
             <CartOtherProductItem />
