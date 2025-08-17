ab29652f5c79e620f21b6e6d29aa6cbc26567e5c
Abdallah Ramadan <abdallahram777@gmail.com>
2025-08-17 18:11:50 +0300

    Save local changes before pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import './App.css'
 import Cart from './pages/Cart'
-import Login from './components/Login'
-import Register from './components/Register'
+import Login from './pages/Login'
+import Register from './pages/Register'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Router>
 
     </>
   )
