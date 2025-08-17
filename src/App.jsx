260c2383b4424b0410c034ff36f90278ca06ebcc
Abdallah Ramadan <abdallahram777@gmail.com>
2025-08-17 18:07:45 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
-
-
 import './App.css'
-<<<<<<< HEAD
 import Cart from './pages/Cart'
 import Login from './components/Login'
 import Register from './components/Register'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-=======
 import ProductDetails from './pages/ProductDetails'
->>>>>>> 3fa5cac (completing page ui)
 
 function App() {
 
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Router>
 
     </>
   )
