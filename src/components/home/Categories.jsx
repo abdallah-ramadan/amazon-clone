6f70209b7a6c1273c571518bf932b55c9a57a970
Abdallah Ramadan <abdallahram777@gmail.com>
2025-08-20 23:49:30 +0300

    Updated the homepage by adding categories and fetching products

--- src/components/home/Categories.jsx
+++ src/components/home/Categories.jsx
@@ -1,31 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Categories.css';
-// import { useNavigate } from "react-router-dom";
+import categoryData from '../../../categoryData.json';
+import { NavLink } from 'react-router-dom';
+
 export default function Categories() {
-    // const navigate = useNavigate();
-    const categoryData = [];
+
+    const [expanded, setExpanded] = useState({});
+
+    const toggleExpand = (id) => {
+        setExpanded((prev) => ({
+            ...prev,
+            [id]: !prev[id]
+        }));
+    };
 
     return (
-        <div className='container category'>
-            <div className='row'>
-                {categoryData.map((category) => (
-                    <div className='col-lg-3 col-md-4 col-sm-6 mb-3' key={category.id}>
-                        <div className='card p-3 h-100'>
-                            <h5 className='card-title'>{category.plot}</h5>
-                            <div className='card-body row'>
-                                {category.subcategories.map((ele) => {
-                                    return (
-                                        <div className='category-item col-6 d-flex flex-column justify-content-center align-items-center' key={ele.category} onClick={() => ``}>
-                                            <img src={ele.img} alt={ele.title} className='img-fluid' />
-                                            <h6 className='my-2'>{ele.title}</h6>
+        <div className="tw-px-5 category tw--mt-[50px] lg:tw--mt-[230px] md:tw--mt-[150px]">
+            <div className="row">
+                {categoryData.map((category) => {
+                    const isExpanded = expanded[category.id] || false;
+                    const visibleSubs = isExpanded
+                        ? category.subcategories
+                        : category.subcategories.slice(0, 4);
+
+                    return (
+                        <div
+                            className="col-lg-3 col-md-4 col-sm-6 mb-3"
+                            key={category.id}
+                        >
+                            <div className="card p-3 h-100 tw-rounded-none">
+                                <h5 className=" tw-font-bold tw-text-xl">{category.plot}</h5>
+                                <div className="row">
+                                    {visibleSubs.map((ele) => (
+                                        <NavLink
+                                            className="category-item col-6 d-flex flex-column justify-content-center align-items-center hover:tw-text-yellow-600"
+                                            key={ele.category}
+                                            to={`/products/${ele.category}`}
+                                        >
+                                            <img
+                                                src={ele.img}
+                                                alt={ele.title}
+                                                className="img-fluid"
+                                            />
+                                            <h6 className="my-2 tw-text-xs">{ele.title}</h6>
+                                        </NavLink>
+                                    ))}
+
+                                    {category.subcategories.length > 4 && (
+                                        <div className="">
+                                            <button
+                                                className=" hover:tw-text-yellow-600 tw-font-bold tw-text-sm"
+                                                onClick={() => toggleExpand(category.id)}
+                                            >
+                                                {isExpanded ? 'See Less' : 'See More'}
+                                            </button>
                                         </div>
-                                    );
-                                })}
+                                    )}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    );
+                })}
             </div>
         </div>
-    )
+    );
 }
