aee578b3e788a634f64a8f30331c58ee7d57394e
Abdallah Ramadan <abdallahram777@gmail.com>
2025-08-23 00:44:40 +0300

    Delete src directory

--- src/components/Loader.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export const Loader = () => (
-    <div className="w-100 d-flex justify-content-center align-items-center" style={{ height: "80vh" }}>
-        <div className="spinner-border" role="status">
-            <span className="visually-hidden">Loading...</span>
-        </div>
-    </div>
-);
